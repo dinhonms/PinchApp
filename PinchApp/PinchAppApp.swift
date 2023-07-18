7bc79c2b96215329d4e9dfb6da6323b099a7204d
Nonato Sousa <nonato.sousa@antikytera.com>
2023-07-18 09:05:49 -0300

    Initial Commit

---
+++ PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Nonato Sousa on 18/07/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
