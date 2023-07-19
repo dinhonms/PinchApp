63d22917375fce32545b46b59d111f971da0fe5b
Nonato Sousa <nonato.sousa@antikytera.com>
2023-07-19 07:54:36 -0300

    * double tap gesture to zoom

--- PinchApp/Screens/ContentView.swift
+++ PinchApp/Screens/ContentView.swift
@@ -8,17 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    //MARK: - PROPERTIES
+    @State var isAnimating = false
+    @State var imageScale: CGFloat = 1
+    
+    //MARK: - FUNCTION
+    
+    //MARK: - CONTENT
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        NavigationView{
+            ZStack{
+                
+                //MARK: - PAGE IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black, radius: 10, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2, perform: {
+                        withAnimation(.spring()) {
+                            
+                            if imageScale == 1{
+                                imageScale = 5
+                            }
+                            else {
+                                imageScale = 1
+                            }
+                        }
+                    })
+                
+            }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.linear(duration: 1)){
+                    isAnimating = true
+                }
+            }
+            
+        }//: NAVIGATION
+        .navigationViewStyle(.columns)
     }
 }
 
+//MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
